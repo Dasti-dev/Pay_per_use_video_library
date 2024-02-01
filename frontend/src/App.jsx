e785cbd0068fc537fedab30c33d534ab079e27b3
Dasti-dev <20cs3015@rgipt.ac.in>
2024-02-02 05:01:25 +0530

    Minor editing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React from 'react';
 import './App.css'
 import { useSelector } from 'react-redux';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
